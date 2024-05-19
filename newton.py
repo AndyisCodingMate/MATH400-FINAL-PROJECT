22771bf91a672c8ceba437d0632cea0602feec52
Andy Poon <andypoon923@gemtrifi.com>
2024-05-18 17:39:35 -0700

    Created blank slate

---
+++ newton.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.linalg import lu_factor, lu_solve
+
+
+
+# Obtain user input
+
+# Obtain second order differential equation 
+
+# Newton's method -> Non-Linear Parts
+
+# LU Decomposition -> Linear Parts
+
+
